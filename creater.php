a887a6a0735425bb10c323af93e467d646142f3d
Randy <rwaterfall531@gmail.com>
2019-11-28 18:09:45 +0800

    push

--- creater.php
+++ creater.php
@@ -1,15 +1,20 @@
 <?php
-include __DIR__ . DIRECTORY_SEPARATOR . 'Controller.php';
+include __DIR__.DIRECTORY_SEPARATOR.'Controller.php';
 $c = new Controller();
+
+$name     = 'testdsa';
+$linkName = $name.'_by_MakeStickersWithRandyBot';
 
 $return = $c->call(
     "{$c->apiUrl}/{$c->botKey}/createnewstickerset",
     [
-        'user_id'=>$c->userID,
-        'name' => 'testdsa_by_MakeStickersWithRandyBot',//英文開頭 _by_MakeStickersWithRandyBot 結尾 、 shardLink
-        'title' => 'meowww',//showName
+        'user_id'      => $c->userID,
+        'name'         => $linkName,//英文開頭 _by_MakeStickersWithRandyBot 結尾 、 shardLink
+        'title'        => $name,//showName
         'png_stickers' => 'tgImage/tg_157778390.png.png', //firstSticker
-        'emojis' => '😀',
-    ],"POST"
+        'emojis'       => '😀',
+    ], "POST"
 );
 echo PHP_EOL.$return.PHP_EOL;
+
+echo "https://t.me/addstickers/".$linkName;
