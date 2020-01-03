91aa0cb53366fd787468dde25170415a25d59c8e
Randy <rwaterfall531@gmail.com>
2020-01-03 17:12:38 +0800

    type

--- creater.php
+++ creater.php
@@ -2,12 +2,13 @@
 include __DIR__.DIRECTORY_SEPARATOR.'Controller.php';
 
 function generateRandomString($length = 10) {
-    $characters = 'abcdefghijklmnopqrstuvwxyz';
+    $characters       = 'abcdefghijklmnopqrstuvwxyz';
     $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
+    $randomString     = '';
+    for ($i = 0 ; $i < $length ; $i++) {
         $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
+
     return $randomString;
 }
 
@@ -19,12 +20,12 @@ $linkName = $name.'_by_MakeStickersWithRandyBot';
 $return = $c->call(
     "{$c->apiUrl}/{$c->botKey}/createnewstickerset",
     [
-        'user_id'      => $c->userID,
-        'name'         => $linkName,//英文開頭 _by_MakeStickersWithRandyBot 結尾 、 shardLink
-        'title'        => $name,//showName
-        'png_sticker' => '@'.realpath('./tgImage/tg_157778398.png.png').';type=image/png', //firstSticker
+        'user_id'     => $c->userID,
+        'name'        => $linkName,//英文開頭 _by_MakeStickersWithRandyBot 結尾 、 shardLink
+        'title'       => $name,//showName
+        'png_sticker' => curl_file_create(realpath('./tgImage/tg_157778398.png.png')), //firstSticker
         //png_sticker:https://f2e-test.baifu-tech.net/tgImage/tg_157778399.png.png
-        'emojis'       => '😀',
+        'emojis'      => '😀',
     ], "POST"
 );
 echo PHP_EOL.$return.PHP_EOL;
